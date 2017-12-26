2396b7631608a015e41233204f523238b7a13ebd
TakaTaka 0 <tk.1987.6.9@gmail.com>
2017-12-26 23:10:47 +0900

    commit every file

---
+++ Login.php
@@ -0,0 +1,91 @@
+<?php
+
+session_start();
+
+$db['host'] = "localhost";
+$db['user'] = "observer";
+$db['pass'] = "test";
+$db['dbname'] = "bullteinBoard";
+
+$errorMessage = "";
+
+if (isset($_POST["login"])) {
+    // 1. ユーザIDの入力チェック
+    if (empty($_POST["userid"])) {  // emptyは値が空のとき
+        $errorMessage = 'ユーザーIDが未入力です。';
+    } else if (empty($_POST["password"])) {
+        $errorMessage = 'パスワードが未入力です。';
+    }
+
+    if (!empty($_POST["userid"]) && !empty($_POST["password"])) {
+        // 入力したユーザIDを格納
+        $userid = $_POST["userid"];
+
+        // 2. ユーザIDとパスワードが入力されていたら認証する
+        $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
+
+        // 3. エラー処理
+        try {
+            $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+            $stmt = $pdo->prepare('SELECT * FROM userData WHERE id = ?');
+            $stmt->execute(array($userid));
+
+            $password = $_POST["password"];
+
+            if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                if (password_verify($password, $row['password'])) {
+                    session_regenerate_id(true);
+
+                    // 入力したIDのユーザー名を取得
+                    $id = $row['id'];
+                    $sql = "SELECT * FROM userData WHERE id = $id";  //入力したIDからユーザー名を取得
+                    $stmt = $pdo->query($sql);
+                    foreach ($stmt as $row) {
+                        $row['name'];  // ユーザー名
+                    }
+                    $_SESSION["NAME"] = $row['name'];
+                    header("Location: Main.php");  // メイン画面へ遷移
+                    exit();  // 処理終了
+                } else {
+                    // 認証失敗
+                    $errorMessage = 'ユーザーIDあるいはパスワードに誤りがあります。';
+                }
+            } else {
+                // 4. 認証成功なら、セッションIDを新規に発行する
+                // 該当データなし
+                $errorMessage = 'ユーザーIDあるいはパスワードに誤りがあります。';
+            }
+        } catch (PDOException $e) {
+            $errorMessage = 'データベースエラー';
+            //$errorMessage = $sql;
+            // $e->getMessage() でエラー内容を参照可能（デバッグ時のみ表示）
+            // echo $e->getMessage();
+        }
+    }
+}
+?>
+
+<html>
+    <head>
+            <meta charset="UTF-8">
+            <title>ログイン</title>
+    </head>
+    <body>
+        <h1>ログイン画面</h1>
+        <form id="loginForm" name="loginForm" action="" method="POST">
+                <legend>ログインフォーム</legend>
+                <div><font color="#ff0000"><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></font></div>
+                <label for="userid">ユーザーID</label><input type="text" id="userid" name="userid" placeholder="ユーザーIDを入力" value="<?php if (!empty($_POST["userid"])) {echo htmlspecialchars($_POST["userid"], ENT_QUOTES);} ?>">
+                <br>
+                <label for="password">パスワード</label><input type="password" id="password" name="password" value="" placeholder="パスワードを入力">
+                <br>
+                <input type="submit" id="login" name="login" value="ログイン">
+        </form>
+        <br>
+        <form action="SignUp.php">
+                <legend>新規登録フォーム</legend>
+                <input type="submit" value="新規登録">
+        </form>
+    </body>
+</html>
---
+++ Main.php
@@ -0,0 +1,102 @@
+<?php
+session_start();
+//require_once("SignUp.php");
+//var_dump($_SESSION);
+
+
+// ログイン状態チェック
+if (!isset($_SESSION["NAME"])) {
+    header("Location: Logout.php");
+    exit;
+}
+
+$signUpMessage = 'あなたのログインIDは' .$_SESSION['USERID']. 'です';
+echo $signUpMessage;
+
+$err_msg1 = "";
+$err_msg2 = "";
+$message ="";
+$name = ( isset( $_POST["name"] ) === true ) ?$_POST["name"]: "";
+$comment  = ( isset( $_POST["comment"] )  === true ) ?  trim($_POST["comment"])  : "";
+
+//$history = (isset( $_POST["history"] ) === true) ?$_POST["history"]: "";
+$history = date("Y/m/d");
+//投稿がある場合のみ処理を行う
+if (  isset($_POST["send"] ) ===  true ) {
+    if ( $name   === "" ) $err_msg1 = "名前を入力してください";
+
+    if ( $comment  === "" )  $err_msg2 = "コメントを入力してください";
+
+    if( $err_msg1 === "" && $err_msg2 === "" ){
+        $fp = fopen( "data.txt" ,"a" );
+        fwrite( $fp ,  $name."\t".$comment."\t".$history."\n");
+        $message ="書き込みに成功しました。";
+    }
+
+}
+
+$fp = fopen("data.txt","r");
+$fpCount = fopen("data.txt","r");
+for ($count=0; fgets($fpCount); $count++){
+//var_dump($count);
+if ($count >=10){
+  fopen("data.txt","w");
+    }
+}
+$dataArr= array();
+while( $res = fgets( $fp)){
+    $tmp = explode("\t",$res);
+    $arr = array(
+        "name"     =>$tmp[0],
+        "comment"  =>$tmp[1],
+        "history"  =>$tmp[2]
+    );
+    $dataArr[]= $arr;
+}
+
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>メイン</title>
+    </head>
+    <body>
+        <h1>メイン画面</h1>
+        <!-- ユーザーIDにHTMLタグが含まれても良いようにエスケープする -->
+        <p>ようこそ<u><?php echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES); ?></u>さん</p>  <!-- ユーザー名をechoで表示 -->
+
+
+        <?php echo $message; ?>
+        <form method="post" action="Main.php">
+        名前：<input type="text" name="name" id="nameId" value="<?php echo $_SESSION["NAME"]; ?>" >
+            <?php echo $err_msg1; ?><br>
+            コメント：<input  name="comment" id="commentId" value="<?php echo $comment; ?>">
+            <?php echo $err_msg2; ?><br>
+        コメント日：<?php echo $history; ?>
+
+<br>
+          <input type="submit" name="send" value="クリック" >
+          <input type="button" name="reset_chk" value="リセット" onclick="clearForm()">
+        </form>
+
+        <script type="text/javascript">
+
+            function clearForm(){
+                document.getElementById('nameId').value="";
+                document.getElementById('commentId').value="";
+            }
+
+        </script>
+        <dl>
+          <?php foreach( $dataArr as $data ):?>
+           <p><span><?php echo $data["name"]; ?></span> : <span><?php echo $data["comment"]; ?></span> @<span><?php echo $data["history"]; ?></span></p>
+          <?php endforeach;?>
+　　　　 </dl>
+        <ul>
+            <li><a href="Logout.php">ログアウト</a></li>
+        </ul>
+    </body>
+</html>
