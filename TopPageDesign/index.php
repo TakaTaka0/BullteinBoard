9810efcee988491612daece4f8a86ed739618563
TakaTaka 0 <tk.1987.6.9@gmail.com>
2018-01-07 15:32:56 +0900

    add Design file

---
+++ TopPageDesign/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" type="text/css" href="./topPage.css"/>
+            <title>Topページ</title>
+    </head>
+    <body>
+    <div id="topContainer">
+    <!--ヘッダ-->
+    <div id="header"><h1><a href="./index.php">TopPage</a></h1></div>
+
+    <!-- メインメニュー -->
+	  <ul id="menu">
+　　　　<li id="menu01"><a href="">Home</a></li>
+　　　　<li id="menu02"><a href=".././login.php">Login</a></li>
+　　　　<li id="menu03"><a href=".././Main.php">Main(NG)</a></li>
+　　　　<li id="menu04"><a href=".././SignUp.php">Sign up</a></li>
+　　　  </ul>
+    </div>
+    </body>
+</html>
